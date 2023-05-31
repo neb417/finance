529342998d06de8109c22d2d215a864e6f712b06
Benjamin Randolphgit config --global user.email neb417@gmail.comgit config --global init.defaultBranch maingit config --global core.editor atom <neb417@gmail.com>
2023-05-31 17:12:50 -0600

    Add incomes to dashboard page

--- spec/features/dashboard_spec.rb
+++ spec/features/dashboard_spec.rb
@@ -6,5 +6,19 @@ RSpec.describe "Dashboard", type: :feature do
       visit root_path
       expect(page).to have_content "Dashboard"
     end
+
+    it 'has incomes on dashboard' do
+      incomes = create_list(:income, 3)
+      income1 = incomes.first
+      income2 = incomes.second
+      income3 = incomes.third
+
+      visit root_path
+
+      expect(page).to have_content income1.income_type
+      expect(page).to have_content income2.income_type
+      expect(page).to have_content income3.income_type
+      expect(page).to have_link "New income"
+    end
   end
 end
