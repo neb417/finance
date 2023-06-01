17d36726c82e9032990927d946f7f753bc436e3f
Benjamin Randolphgit config --global user.email neb417@gmail.comgit config --global init.defaultBranch maingit config --global core.editor atom <neb417@gmail.com>
2023-06-01 16:01:14 -0600

    Create Fixed Expense from dashboard

--- app/models/fixed_expense.rb
+++ app/models/fixed_expense.rb
@@ -17,4 +17,31 @@ class FixedExpense < ApplicationRecord
   monetize :annual_cost_cents
   monetize :monthly_cost_cents
   monetize :bi_weekly_cost_cents
+
+  after_create_commit  -> { broadcast_append_to :fixed_expense, partial: "fixed_expenses/fixed_expense", locals: { fixed_expense: self } }
+  after_update_commit  -> { broadcast_replace_to self }
+  after_destroy_commit -> { broadcast_remove_to :fixed_expense, target: dom_id(self, :index) }
+
+  def self.new_from_dashboard(params:)
+    expense = params[:fixed_expense]
+    amount = (expense[:amount].to_f * 100).to_i
+    case expense[:frequency]
+    when "monthly"
+      FixedExpense.new(
+        expense_name: expense[:expense_name],
+        annual_cost_cents: amount * 12,
+        monthly_cost_cents: amount,
+        bi_weekly_cost_cents: (amount * 12) / 26
+      )
+    when "annually"
+      FixedExpense.new(
+        expense_name: expense[:expense_name],
+        annual_cost_cents: amount,
+        monthly_cost_cents: amount / 12,
+        bi_weekly_cost_cents: amount / 52
+      )
+    else
+      FixedExpense.new
+    end
+  end
 end
