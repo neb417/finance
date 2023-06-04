608d23aabf275570a976117e71ce7252a627a617
Benjamin Randolphgit config --global user.email neb417@gmail.comgit config --global init.defaultBranch maingit config --global core.editor atom <neb417@gmail.com>
2023-06-03 21:52:21 -0600

    Order Fixed expenses

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,7 @@
 class DashboardController < ApplicationController
   def index
     @incomes = Income.all
-    @fixed_expenses = FixedExpense.all
+    @fixed_expenses = FixedExpense.all.order(annual_cost_cents: :desc)
     @totals = FixedExpense.total_costs
   end
 end
