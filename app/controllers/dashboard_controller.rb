ec1a33d079e73bbce1f78a3998317362e813e081
Benjamin Randolphgit config --global user.email neb417@gmail.comgit config --global init.defaultBranch maingit config --global core.editor atom <neb417@gmail.com>
2023-11-06 13:54:45 -0700

    Add concern to build instance variables

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,10 +1,6 @@
 class DashboardController < ApplicationController
+  include DashboardConcern
   def index
-    @incomes = Income.order_by_type
-    @fixed_expenses = FixedExpense.get_ordered
-    @totals = FixedExpense.total_costs
-    @federal_tax_brackets = FederalTaxBracket.order_by_range
-    @salary_taxed = Income.tax_on_income(income_type: "Salary")
-    @hourly_taxed = Income.tax_on_income(income_type: "Hourly")
+    build_instance_variables
   end
 end
