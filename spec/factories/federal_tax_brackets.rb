1bafd95423cb0118659a187ef372871b6c24be1e
Benjamin Randolphgit config --global user.email neb417@gmail.comgit config --global init.defaultBranch maingit config --global core.editor atom <neb417@gmail.com>
2023-11-06 20:03:16 -0500

    Update test suit

--- spec/factories/federal_tax_brackets.rb
+++ spec/factories/federal_tax_brackets.rb
@@ -16,10 +16,38 @@
 #
 FactoryBot.define do
   factory :federal_tax_bracket do
-    tier { Faker::Name.name }
-    bottom_range_cents { Faker::Number.within(range: 1..10) }
-    top_range_cents { Faker::Number.within(range: 11..20) }
-    rate { Faker::Number.within(range: 0.01..0.5) }
-    cumulative_cents { Faker::Number.within(range: 1..20) }
+    tier { 'Tier 1' }
+    bottom_range_cents { 0 }
+    top_range_cents { 100000 }
+    rate { 0.1 }
+    cumulative_cents { 0 }
+
+    trait :tier_2 do
+      tier { 'Tier 2' }
+      bottom_range_cents { 1000100 }
+      top_range_cents { 10000000 }
+      rate { 0.15 }
+      cumulative_cents { 200000 }
+    end
+
+    trait :tier_3 do
+      tier { 'Tier 2' }
+      bottom_range_cents { 10000100 }
+      top_range_cents { 50000000 }
+      rate { 0.25 }
+      cumulative_cents { 500000 }
+    end
+
+    # trait :with_all_tiers do
+    #   after :create do |_record|
+    #     create(:federal_tax_bracket, :tier_2)
+    #     create(:federal_tax_bracket, :tier_3)
+    #   end
+    # end
+    #
+    # to_create do |instance|
+    #   instance.id = FederalTaxBracket.find_or_create_by(tier: instance.tier).id
+    #   instance.reload
+    # end
   end
 end
--- spec/features/dashboard/dashboard_spec.rb
+++ spec/features/dashboard/dashboard_spec.rb
@@ -1,6 +1,13 @@
 require "rails_helper"
 
 RSpec.describe "Dashboard", type: :feature do
+  let!(:income1) { create(:income)}
+  let!(:income2) { create(:income, :hourly)}
+  let!(:fed1) { create(:federal_tax_bracket)}
+  let!(:fed2) { create(:federal_tax_bracket, :tier_2)}
+  let!(:fed3) { create(:federal_tax_bracket, :tier_3)}
+  let!(:fixed_expenses) { create_list(:fixed_expense, 2)}
+
   describe "GET /index" do
     it "routes to root path" do
       visit root_path
@@ -8,21 +15,17 @@ RSpec.describe "Dashboard", type: :feature do
     end
 
     it "has incomes on dashboard" do
-      incomes = create_list(:income, 3)
-      income1 = incomes.first
-      income2 = incomes.second
-      income3 = incomes.third
-
       visit root_path
 
       expect(page).to have_content income1.income_type
       expect(page).to have_content income2.income_type
-      expect(page).to have_content income3.income_type
       expect(page).to have_link "New Income"
     end
 
     it "has fixed expenses on the dashboard" do
-      fixed_expenses = create_list(:fixed_expense, 2)
+      # create(:income, :with_all_types)
+      # create(:federal_tax_bracket, :with_all_tiers)
+      # fixed_expenses = create_list(:fixed_expense, 2)
       fixed1 = fixed_expenses.first
       fixed2 = fixed_expenses.last
 
