7b7f8baaac00fecfb69d51e4b68cd5453d47ed54
Benjamin Randolph <104036158+neb417@users.noreply.github.com>
2023-11-06 18:13:46 -0700

    Revert "Assign variables" (#12)
    * Create budget template
    
    * Render budget on dashboard
    
    * Update budget with turbo
    
    * Lint
    
    * Assign variables (#10)
    
    * Add concern to build instance variables
    
    * Lint
    
    * Revert "Assign variables (#10)"
    
    This reverts commit 556a050b6c7692f8d075493552730a981bdd5c73.

--- app/controllers/fixed_expenses_controller.rb
+++ app/controllers/fixed_expenses_controller.rb
@@ -27,6 +27,8 @@ class FixedExpensesController < ApplicationController
       if @fixed_expense.save
         @totals = FixedExpense.total_costs
         @fixed_expenses = FixedExpense.get_ordered
+        @salary_taxed = Income.tax_on_income(income_type: "Salary")
+        @hourly_taxed = Income.tax_on_income(income_type: "Hourly")
         format.html { redirect_to root_path, notice: "Fixed expense was successfully created." }
         format.turbo_stream
       else
@@ -41,6 +43,8 @@ class FixedExpensesController < ApplicationController
     respond_to do |format|
       if @fixed_expense.update_from_dashboard(params: params[:fixed_expense])
         @totals = FixedExpense.total_costs
+        @salary_taxed = Income.tax_on_income(income_type: "Salary")
+        @hourly_taxed = Income.tax_on_income(income_type: "Hourly")
         format.html { redirect_to root_path, notice: "Fixed expense was successfully updated." }
         format.turbo_stream
       else
@@ -55,6 +59,8 @@ class FixedExpensesController < ApplicationController
     @fixed_expense.destroy
     @totals = FixedExpense.total_costs
     @fixed_expenses = FixedExpense.get_ordered
+    @salary_taxed = Income.tax_on_income(income_type: "Salary")
+    @hourly_taxed = Income.tax_on_income(income_type: "Hourly")
     respond_to do |format|
       format.html { redirect_to fixed_expenses_path, notice: "Fixed expense was successfully destroyed." }
       format.turbo_stream
