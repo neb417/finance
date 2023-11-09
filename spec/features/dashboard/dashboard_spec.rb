0d6c15d50b1d039ef31119986184c9651c00ac7d
Benjamin Randolph <104036158+neb417@users.noreply.github.com>
2023-11-09 07:59:50 -0700

    Clean up spacings and columns (#13)
    * Clean up spacings and columns
    
    * Update test

--- spec/features/dashboard/dashboard_spec.rb
+++ spec/features/dashboard/dashboard_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe "Dashboard", type: :feature do
   describe "GET /index" do
     it "routes to root path" do
       visit root_path
-      expect(page).to have_content "Dashboard"
+      expect(page).to have_content "Budget Calculator"
     end
 
     it "has incomes on dashboard" do
