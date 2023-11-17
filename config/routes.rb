b7542bcf7245c5c80f46eff6843c68f0f0e963f3
Benjamin Randolphgit config --global user.email neb417@gmail.comgit config --global init.defaultBranch maingit config --global core.editor atom <neb417@gmail.com>
2023-11-17 16:19:34 -0700

    Migrate savings rate

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
+  resources :savings_rates
   resources :federal_tax_brackets
   resources :fixed_expenses
   root "dashboard#index"
   resources :incomes
   post "/income_switch", to: "incomes#income_switch"
-  post "/saving_rate", to: "savings#saving_rate"
-  post "/investing_rate", to: "savings#investing_rate"
 end
