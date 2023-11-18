79c22c32b8b1d65ea025ecb9963238651233b8be
Benjamin Randolphgit config --global user.email neb417@gmail.comgit config --global init.defaultBranch maingit config --global core.editor atom <neb417@gmail.com>
2023-11-18 15:10:28 -0700

    Add service and concern

---
+++ app/services/guilt_free_calculator.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class GuiltFreeCalculator
+  def initialize(taxed_income, savings_totals)
+    @taxed_income = taxed_income
+    @savings_totals = savings_totals
+  end
+end
