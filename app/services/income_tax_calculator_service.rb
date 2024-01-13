2decd70609b40bc52a6faadf6b583c9a7c91659d
Benjamin Randolph <neb417@gmail.com>
2024-01-13 07:27:45 -0700

    Calculate taxes for self-employed.

--- app/services/income_tax_calculator_service.rb
+++ app/services/income_tax_calculator_service.rb
@@ -1,23 +1,29 @@
 class IncomeTaxCalculatorService
   attr_reader :income,
-    :annual_income,
-    :federal_tax,
-    :fica_tax,
-    :net_after_fed_tax,
-    :state_tax,
-    :total_net_income,
-    :bi_weekly_net_income,
-    :daily_income,
-    :monthly_income,
-    :weekly_income,
-    :quarterly_income,
-    :biannual_income
+              :annual_income,
+              :fica_tax,
+              :agi_deduction,
+              :agi,
+              :federal_income_tax,
+              :total_federal_tax,
+              :net_after_fed_tax,
+              :state_tax,
+              :total_net_income,
+              :bi_weekly_net_income,
+              :daily_income,
+              :monthly_income,
+              :weekly_income,
+              :quarterly_income,
+              :biannual_income
 
   def initialize(income:)
     @income = income
     @annual_income = income.weekly_income * 52
-    @federal_tax = calculate_fed_tax
     @fica_tax = calculate_fica_tax
+    @agi_deduction = calculate_agi_deduction
+    @agi = calculate_agi
+    @federal_income_tax = calculate_fed_tax
+    @total_federal_tax = calculate_total_federal_tax
     @net_after_fed_tax = calculate_net_after_fed_tax
     @state_tax = calculate_state_tax
     @total_net_income = calculate_total_net_income
@@ -31,23 +37,35 @@ class IncomeTaxCalculatorService
 
   private
 
+  def calculate_fica_tax
+    @annual_income * 0.153
+  end
+
+  def calculate_agi_deduction
+    @fica_tax * 0.5
+  end
+
+  def calculate_agi
+    @annual_income - @agi_deduction
+  end
+
   def calculate_fed_tax
-    bracket = FederalTaxBracket.where("bottom_range_cents <= ?", @annual_income.cents).order(:bottom_range_cents).last
-    taxable_at_bracket_rate = Money.new(@annual_income.cents - bracket.bottom_range_cents)
+    bracket = FederalTaxBracket.where("bottom_range_cents <= ?", @agi.cents).order(:bottom_range_cents).last
+    taxable_at_bracket_rate = Money.new(@agi - bracket.bottom_range)
     rated = bracket.rate * taxable_at_bracket_rate
     rated + bracket.cumulative
   end
 
-  def calculate_fica_tax
-    @annual_income * 0.0765
+  def calculate_total_federal_tax
+    @fica_tax + @federal_income_tax
   end
 
   def calculate_state_tax
-    @net_after_fed_tax * 0.0463
+    @net_after_fed_tax * 0.044
   end
 
   def calculate_net_after_fed_tax
-    @annual_income - @federal_tax
+    @agi - @federal_income_tax
   end
 
   def calculate_total_net_income
