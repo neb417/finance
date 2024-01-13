06fc825501f6440d043203097faa2c37e3d6bec0
Benjamin Randolph <104036158+neb417@users.noreply.github.com>
2024-01-13 07:55:00 -0700

    Refactor federal tax (#35)
    * Calculate taxes for self-employed.
    
    * Add tests.
    
    * Lint.

--- spec/factories/federal_tax_brackets.rb
+++ spec/factories/federal_tax_brackets.rb
@@ -18,32 +18,32 @@ FactoryBot.define do
   factory :federal_tax_bracket do
     tier { "Tier 1" }
     bottom_range_cents { 0 }
-    top_range_cents { 100000 }
+    top_range_cents { 100_000 }
     rate { 0.1 }
     cumulative_cents { 0 }
 
     trait :tier_2 do
       tier { "Tier 2" }
-      bottom_range_cents { 1000100 }
-      top_range_cents { 10000000 }
+      bottom_range_cents { 1_000_100 }
+      top_range_cents { 10_000_000 }
       rate { 0.15 }
-      cumulative_cents { 200000 }
+      cumulative_cents { 200_000 }
     end
 
     trait :tier_3 do
       tier { "Tier 2" }
-      bottom_range_cents { 10000100 }
-      top_range_cents { 50000000 }
+      bottom_range_cents { 10_000_100 }
+      top_range_cents { 50_000_000 }
       rate { 0.25 }
-      cumulative_cents { 500000 }
+      cumulative_cents { 500_000 }
     end
 
-    # trait :with_all_tiers do
-    #   after :create do |_record|
-    #     create(:federal_tax_bracket, :tier_2)
-    #     create(:federal_tax_bracket, :tier_3)
-    #   end
-    # end
+    trait :with_all_tiers do
+      after :create do |_record|
+        create(:federal_tax_bracket, :tier_2)
+        create(:federal_tax_bracket, :tier_3)
+      end
+    end
     #
     # to_create do |instance|
     #   instance.id = FederalTaxBracket.find_or_create_by(tier: instance.tier).id
