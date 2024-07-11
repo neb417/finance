edca4e1e8f8f9f2cf4776f61e17e3394bdee2d3c
Benjamin Randolph <neb417@gmail.com>
2024-07-11 07:40:52 -0600

    Fix hourly income rate in data migration.

--- db/data/20240711125841_create_incomes.rb
+++ db/data/20240711125841_create_incomes.rb
@@ -3,7 +3,7 @@
 class CreateIncomes < ActiveRecord::Migration[7.0]
   def up
     Income.create(income_type: "Salary", rate: 100000, hours: 40, weekly_income: 1923.08)
-    Income.create(income_type: "Hourly", rate: 4700, hours: 40, weekly_income: 1880.0)
+    Income.create(income_type: "Hourly", rate: 47, hours: 40, weekly_income: 1880.0)
   end
 
   def down
