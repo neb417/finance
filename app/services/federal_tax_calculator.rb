4d8253e9c70eb5ba25ea3501c4acd88613d35a11
Benjamin Randolph <neb417@gmail.com>
2024-07-15 06:53:40 -0600

    Add standard deduction to federal tax calculator.

--- app/services/federal_tax_calculator.rb
+++ app/services/federal_tax_calculator.rb
@@ -14,9 +14,14 @@ class FederalTaxCalculator
   attr_accessor :income
 
   def calculate
-    bracket = FederalTaxBracket.where("bottom_range_cents <= ?", income.fractional).order(:bottom_range_cents).last
-    taxable_at_bracket_rate = Money.new(income - bracket.bottom_range)
+    bracket = FederalTaxBracket.where("bottom_range_cents <= ?", taxable_income.fractional).order(:bottom_range_cents).last
+    taxable_at_bracket_rate = Money.new(taxable_income - bracket.bottom_range)
     rated = bracket.rate * taxable_at_bracket_rate
     rated + bracket.cumulative
+  end
+
+  def taxable_income
+    # 2024 standard deduction = 13,850
+    @taxable_income ||= income - Money.new(13_850_00)
   end
 end
