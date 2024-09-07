45c97e1786ad6ea1450777ef04fe37084217f25c
Benjamin Randolph <neb417@gmail.com>
2024-09-07 06:52:45 -0600

    Update specs.

--- spec/services/federal_tax_calculator_spec.rb
+++ spec/services/federal_tax_calculator_spec.rb
@@ -11,16 +11,14 @@ RSpec.describe FederalTaxCalculator, type: :service do
 
   let!(:salary_income) { create(:income) }
   let!(:tax_brackets) { create(:federal_tax_bracket, :with_all_tiers) }
-  let!(:standard_deduction) { create(:standard_deduction) }
 
   it { expect(service).to be_a Money }
 
   it "calculates federal tax" do
     # salary_income = $50,000
-    # standard_deduction = $13,850
     # tax_brackets = 10% on first $1,000, 15% from $1,001 to $100,000, 25% from $100,001 to $500,000
 
-    expect(service.format).to eq("$5,372.35")
-    expect((salary_income.rate - service).format).to eq("$44,627.65")
+    expect(service.format).to eq("$7,449.85")
+    expect((salary_income.rate - service).format).to eq("$42,550.15")
   end
 end
