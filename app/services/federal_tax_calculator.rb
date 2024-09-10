c6c78d2865791667aaed976f8f385d385c8cde0f
Benjamin Randolph <104036158+neb417@users.noreply.github.com>
2024-09-10 06:45:38 -0600

    Update specs. (#45)

--- app/services/federal_tax_calculator.rb
+++ app/services/federal_tax_calculator.rb
@@ -14,14 +14,9 @@ class FederalTaxCalculator
   attr_accessor :income
 
   def calculate
-    bracket = FederalTaxBracket.where("bottom_range_cents <= ?", taxable_income.fractional).order(:bottom_range_cents).last
-    taxable_at_bracket_rate = Money.new(taxable_income - bracket.bottom_range)
+    bracket = FederalTaxBracket.where("bottom_range_cents <= ?", income.fractional).order(:bottom_range_cents).last
+    taxable_at_bracket_rate = Money.new(income - bracket.bottom_range)
     rated = bracket.rate * taxable_at_bracket_rate
     rated + bracket.cumulative
-  end
-
-  def taxable_income
-    # 2024 standard deduction = 13,850
-    @taxable_income ||= income - StandardDeduction.first.amount
   end
 end
