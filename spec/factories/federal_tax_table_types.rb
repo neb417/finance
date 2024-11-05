c13bf90f3780137dde1954842276046e9cdc1f88
Benjamin Randolph <neb417@gmail.com>
2024-11-05 10:18:06 -0700

    Add factories for specs.

--- spec/factories/federal_tax_table_types.rb
+++ spec/factories/federal_tax_table_types.rb
@@ -9,5 +9,26 @@
 #
 FactoryBot.define do
   factory :federal_tax_table_type do
+    filing_status { "single" }
+
+    trait :married_filing_jointly do
+      filing_status { "married_filing_jointly" }
+    end
+
+    trait :married_filing_separately do
+      filing_status { "married_filing_separately" }
+    end
+
+    trait :head_of_household do
+      filing_status { "head_of_household" }
+    end
+
+    trait :with_all_statuses do
+      after :create do |_record|
+        create(:federal_tax_table_type, :married_filing_jointly)
+        create(:federal_tax_table_type, :married_filing_separately)
+        create(:federal_tax_table_type, :head_of_household)
+      end
+    end
   end
 end
